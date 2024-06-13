26dcd8789847ca8cf19da0496c6d13c94796722c
firdausyifa <98065642+firdausyifa@users.noreply.github.com>
2024-06-13 18:12:30 +0700

    Update routeHandler.js

--- src/handlers/routeHandler.js
+++ src/handlers/routeHandler.js
@@ -1,15 +1,20 @@
-const googleMaps = require('../utils/googleMaps');
-const machineLearning = require('../utils/machineLearning');
+const axios = require('axios');
+const { googleMapsApiKey } = require('../utils/mapsConfig');
 
-async function getOptimalRoute(req, res) {
+exports.getOptimalRoute = async (req, res) => {
   const { origin, destinations } = req.body;
- try {
-    const routes = await googleMaps.getRoutes(origin, destinations);
-    const optimalRoute = await machineLearning.getOptimalRoute(routes);
-    res.status(200).send(optimalRoute);
-  } catch (error) {
-    res.status(400).send({ message: 'Error getting optimal route' });
-  }
-}
 
-module.exports = { getOptimalRoute };
+  try {
+    const response = await axios.get(`https://maps.googleapis.com/maps/api/directions/json`, {
+      params: {
+        origin,
+        destination: origin,
+        waypoints: `optimize:true|${destinations.join('|')}`,
+        key: googleMapsApiKey
+      }
+    });
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
