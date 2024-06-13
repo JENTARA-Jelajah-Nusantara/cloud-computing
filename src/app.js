6734c31c318a5833b19b7c510e83834dc9fcfde0
firdausyifa <98065642+firdausyifa@users.noreply.github.com>
2024-06-13 18:15:21 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const config = require('./utils/mapsConfig');
+
+const authRoutes = require('./routes/auth');
+const searchRoutes = require('./routes/search');
+const placeDetailsRoutes = require('./routes/placeDetails');
+const optimalRouteRoutes = require('./routes/routes');
+const reviewsRoutes = require('./routes/reviews');
+
 const app = express();
-const { authRouter, reviewsRouter, routesRouter } = require('./routes');
 
 app.use(express.json());
-app.use('/api/auth', authRouter);
-app.use('/api/reviews', reviewsRouter);
-app.use('/api/routes', routesRouter);
+app.use('/api/auth', authRoutes);
+app.use('/api/services', searchRoutes);
+app.use('/api/services', placeDetailsRoutes);
+app.use('/api/services', optimalRouteRoutes);
+app.use('/api/services', reviewsRoutes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
