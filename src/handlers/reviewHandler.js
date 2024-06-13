cbd67f56c1e657ed349223561d683509dbda61cc
firdausyifa <98065642+firdausyifa@users.noreply.github.com>
2024-06-13 18:12:09 +0700

    Update reviewHandler.js

--- src/handlers/reviewHandler.js
+++ src/handlers/reviewHandler.js
@@ -1,22 +1,19 @@
-const { firebase, db } = require('../utils/firebase');
+const axios = require('axios');
+const { googleMapsApiKey } = require('../utils/mapsConfig');
 
-async function addReview(req, res) {
-  const { userId, rating, review } = req.body;
+exports.getReviews = async (req, res) => {
+  const { place_id } = req.query;
+
   try {
-    await db.collection('reviews').add({ userId, rating, review });
-    res.status(201).send({ message: 'Review added successfully' });
+    const response = await axios.get(`https://maps.googleapis.com/maps/api/place/details/json`, {
+      params: {
+        place_id,
+        fields: 'reviews',
+        key: googleMapsApiKey
+      }
+    });
+    res.json(response.data.result.reviews);
   } catch (error) {
-    res.status(400).send({ message: 'Error adding review' });
+    res.status(500).json({ error: error.message });
   }
-}
-
-async function getReviews(req, res) {
-  try {
-    const reviews = await db.collection('reviews').get();
-    res.status(200).send(reviews.docs.map(doc => doc.data()));
-  } catch (error) {
-    res.status(400).send({ message: 'Error getting reviews' });
-  }
-}
-
-module.exports = { addReview, getReviews };
+};
